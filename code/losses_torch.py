27a878db470f9bd2c06f48ae9136ca79450b016c
Kirill Dogadin <kirillsc2@gmail.com>
2020-12-07 12:58:31 +0100

    [ADD] losses

--- code/losses_torch.py
+++ code/losses_torch.py
@@ -1,4 +1,4 @@
-from utils import *
+import torch
 
 
 def relation_logistic_loss(phs, prs, pts, nhs, nrs, nts):
@@ -10,3 +10,19 @@ def relation_logistic_loss(phs, prs, pts, nhs, nrs, nts):
     neg_loss = torch.sum(torch.log(1 + torch.exp(neg_score)))
     loss = torch.add(pos_loss, neg_loss)
     return loss
+
+
+def logistic_loss_wo_negs(phs, pas, pvs, pws):
+    pos_distance = phs + pas - pvs
+    pos_score = -torch.sum(torch.square(pos_distance), dim=1)
+    pos_score = torch.log(1 + torch.exp(-pos_score))
+    pos_score = torch.multiply(pos_score, pws)
+    pos_loss = torch.sum(pos_score)
+    return pos_loss
+
+
+def relation_logistic_loss_wo_negs(phs, prs, pts):
+    pos_distance = phs + prs - pts
+    pos_score = -torch.sum(torch.square(pos_distance), dim=1)
+    pos_loss = torch.sum(torch.log(1 + torch.exp(-pos_score)))
+    return pos_loss
